ecdade7bb63b361f7fb7473e7ba58cbc2956a554
chancestrickland <chance.strickland@gmail.com>
2020-02-23 15:35:30 -0800

    udpdate deps and configs

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,9 @@ module.exports = {
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining',
   ],
 };
